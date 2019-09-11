f8ba9b22f0e66b5c54abc6fa4d2d593e92d68ba6
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-09-11 13:11:59 +0300

    Add module1 project Add bean classes

---
+++ tasks/module1/src/main/java/by/training/module1/bean/Gemstone.java
@@ -0,0 +1,49 @@
+package by.training.module1.bean;
+
+public class Gemstone {
+    private String gemType;
+    private String gemName;
+    private double value;
+    private double weight;
+    private double transparency;
+
+    public String getGemType() {
+        return gemType;
+    }
+
+    public void setGemType(String gemType) {
+        this.gemType = gemType;
+    }
+
+    public String getGemName() {
+        return gemName;
+    }
+
+    public void setGemName(String gemName) {
+        this.gemName = gemName;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getTransparency() {
+        return transparency;
+    }
+
+    public void setTransparency(double transparency) {
+        this.transparency = transparency;
+    }
+}
---
+++ tasks/module1/src/main/java/by/training/module1/bean/Necklace.java
@@ -0,0 +1,15 @@
+package by.training.module1.bean;
+
+import java.util.List;
+
+public class Necklace {
+    private List<Gemstone> gemstones;
+
+    public List<Gemstone> getGemstones() {
+        return gemstones;
+    }
+
+    public void setGemstones(List<Gemstone> gemstones) {
+        this.gemstones = gemstones;
+    }
+}
