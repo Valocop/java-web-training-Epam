27af3a9539da941a919a8e336d6d5acab3afbf43
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-09-12 15:39:03 +0300

    update project

--- tasks/module1/src/main/java/by/training/module1/command/CalculateValueCommand.java
+++ tasks/module1/src/main/java/by/training/module1/command/CalculateValueCommand.java
@@ -1,9 +1,26 @@
 package by.training.module1.command;
 
+import by.training.module1.entity.Necklace;
+import by.training.module1.service.NecklaceService;
+import by.training.module1.service.ServiceFactory;
+import by.training.module1.service.exception.ServiceException;
+
 public class CalculateValueCommand implements Command {
+    private ServiceFactory serviceFactory = ServiceFactory.getInstance();
+    private NecklaceService necklaceService = serviceFactory.getNecklaceService();
+    private Necklace necklace;
+
+    CalculateValueCommand(Necklace necklace) {
+        this.necklace = necklace;
+    }
 
     @Override
     public void execute() {
-
+        try {
+            necklaceService.calculateValue(necklace);
+        } catch (ServiceException e) {
+            System.out.println(e.getMessage());
+//            log
+        }
     }
 }
