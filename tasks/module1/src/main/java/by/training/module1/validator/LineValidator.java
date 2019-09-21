1ea3f63f3b020f144226a6f4d8f3ccce7d8e2386
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-09-21 15:25:42 +0300

    add LineValidator

---
+++ tasks/module1/src/main/java/by/training/module1/validator/LineValidator.java
@@ -0,0 +1,36 @@
+package by.training.module1.validator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LineValidator {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private String line;
+    private ResultValidator resultValidator = new ResultValidator();
+
+    public LineValidator(String line) {
+        this.line = line;
+    }
+
+    private void checkLine() {
+        Pattern pattern = Pattern.compile("( *[\\wа-яА-Я.]+ *: *[\\wа-яА-Я.]+ *)");
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+            LOGGER.info("[" + line + "]" + " contain parameters.");
+        } else {
+            resultValidator.addException("InvalidLine", Arrays.asList("[" + line + "]" + " do not contain parameters."));
+            LOGGER.info("[" + line + "]" + " do not contain parameters.");
+        }
+    }
+
+    public ResultValidator validate() {
+        checkLine();
+        return resultValidator;
+    }
+}
+
