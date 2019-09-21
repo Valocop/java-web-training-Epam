44ffde7e30966085864520333380a4a7598cdba2
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-09-21 15:21:05 +0300

    delete incorrect service

--- tasks/module1/src/main/java/by/training/module1/service/DecorMatchSpecification.java
+++
@@ -1,9 +0,0 @@
-package by.training.module1.service;
-
-public interface DecorMatchSpecification<T> {
-    boolean match(T entity);
-
-    default DecorMatchSpecification<T> and(DecorMatchSpecification<T> other) {
-        return entity -> match(entity) && other.match(entity);
-    }
-}
