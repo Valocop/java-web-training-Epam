8a467158898a01a64619ed1301edd746cecc09f4
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-10-20 02:07:09 +0300

    migrate medicineHandler

--- tasks/module3/src/main/java/by/training/module3/handler/Handler.java
+++
@@ -1,9 +0,0 @@
-package by.training.module3.handler;
-
-import org.xml.sax.helpers.DefaultHandler;
-
-import java.util.List;
-
-public abstract class Handler<T> extends DefaultHandler {
-    public abstract List<T> getEntities();
-}
