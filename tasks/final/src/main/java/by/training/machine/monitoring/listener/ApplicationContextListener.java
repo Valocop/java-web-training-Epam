e02bba75fc426df7b920863d36b49be05f31c328
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-12-01 16:28:55 +0300

    Add listeners

---
+++ tasks/final/src/main/java/by/training/machine/monitoring/listener/ApplicationContextListener.java
@@ -0,0 +1,27 @@
+package by.training.machine.monitoring.listener;
+
+import by.training.machine.monitoring.ApplicationContext;
+import by.training.machine.monitoring.message.MessageManager;
+import lombok.extern.log4j.Log4j;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@Log4j
+@WebListener
+public class ApplicationContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ApplicationContext.initialize();
+        servletContextEvent.getServletContext().setAttribute("messageManager", ApplicationContext.getInstance().getBean(MessageManager.class));
+        log.info("Context was initialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ApplicationContext.getInstance().destroy();
+        log.info("Context was destroyed");
+    }
+}
---
+++ tasks/final/src/main/java/by/training/machine/monitoring/listener/ApplicationHttpSessionListener.java
@@ -0,0 +1,28 @@
+package by.training.machine.monitoring.listener;
+
+import by.training.machine.monitoring.SecurityService;
+import lombok.extern.log4j.Log4j;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@Log4j
+@WebListener
+public class ApplicationHttpSessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        log.info("Time of session is timeout");
+        if (SecurityService.getInstance().deleteSession(httpSessionEvent.getSession())) {
+            log.info("Session was deleted");
+        } else {
+            log.warn("Failed to delete session");
+        }
+    }
+}
