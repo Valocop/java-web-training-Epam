96b28cf4589d1433a576d0397ff03cb247184da5
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-12-04 16:23:43 +0300

    Add Application servlet

---
+++ tasks/final/src/main/java/by/training/machine/monitoring/servlet/ApplicationServlet.java
@@ -0,0 +1,39 @@
+package by.training.machine.monitoring.servlet;
+
+import by.training.machine.monitoring.ApplicationContext;
+import by.training.machine.monitoring.command.CommandException;
+import by.training.machine.monitoring.command.ServletCommand;
+import lombok.extern.log4j.Log4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j
+@WebServlet(urlPatterns = "/", loadOnStartup = 1, name = "app")
+public class ApplicationServlet extends HttpServlet {
+    private static final long serialVersionUID = -898419077104540041L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String commandName = req.getParameter("commandName");
+        ServletCommand command = ApplicationContext.getInstance().getBean(commandName);
+        if (command != null) {
+            try {
+                command.execute(req, resp);
+            } catch (CommandException e) {
+                throw new ServletException(e);
+            }
+        } else {
+            req.getRequestDispatcher("/jsp/main.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
