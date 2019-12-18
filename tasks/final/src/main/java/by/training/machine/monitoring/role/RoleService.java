bd8113855fd0ec622c13b6f760125ebc9330ed6d
Andrei Zdanovich <Andrei.Zdanovich@gmail.com>
2019-12-19 02:33:47 +0300

    Refactor role

--- tasks/final/src/main/java/by/training/machine/monitoring/role/RoleService.java
+++ tasks/final/src/main/java/by/training/machine/monitoring/role/RoleService.java
@@ -1,14 +1,15 @@
 package by.training.machine.monitoring.role;
 
 import by.training.machine.monitoring.dao.DaoException;
+import by.training.machine.monitoring.service.ServiceException;
 import by.training.machine.monitoring.user.UserDto;
 
 import java.util.List;
 
 public interface RoleService {
     List<RoleDto> getUserRoles(UserDto user) throws DaoException;
-    void assignRoles(UserDto user, List<RoleDto> roles) throws DaoException;
-    void assignDefaultRoles(Long user) throws DaoException;
+    void assignRoles(UserDto user, List<RoleDto> roles) throws ServiceException;
+    void assignDefaultRoles(Long user) throws ServiceException;
     List<RoleDto> findAllRoles();
     boolean saveRole(RoleDto role);
     RoleDto getRole(Long id);
--- tasks/final/src/main/java/by/training/machine/monitoring/role/RoleServiceImpl.java
+++ tasks/final/src/main/java/by/training/machine/monitoring/role/RoleServiceImpl.java
@@ -1,11 +1,14 @@
 package by.training.machine.monitoring.role;
 
+import by.training.machine.monitoring.app.ApplicationConstant;
 import by.training.machine.monitoring.core.Bean;
 import by.training.machine.monitoring.dao.DaoException;
 import by.training.machine.monitoring.dao.TransactionSupport;
 import by.training.machine.monitoring.dao.Transactional;
+import by.training.machine.monitoring.manufacture.ManufactureDao;
 import by.training.machine.monitoring.manufacture.ManufactureDto;
-import by.training.machine.monitoring.manufacture.ManufactureService;
+import by.training.machine.monitoring.service.ServiceException;
+import by.training.machine.monitoring.user.UserDao;
 import by.training.machine.monitoring.user.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -20,32 +23,37 @@ import java.util.Optional;
 @TransactionSupport
 @AllArgsConstructor
 public class RoleServiceImpl implements RoleService {
-    private ManufactureService manufactureService;
     private RoleDao roleDao;
+    private UserDao userDao;
+    private ManufactureDao manufactureDao;
 
     @Override
-    public List<RoleDto> getUserRoles(UserDto user) throws DaoException {
-        return roleDao.getUserRoles(user.getId());
-    }
-
-    @Override
-    @Transactional
-    public void assignRoles(UserDto user, List<RoleDto> roles) throws DaoException {
+    public List<RoleDto> getUserRoles(UserDto user) {
         try {
-            for (RoleDto role : roles) {
-                roleDao.assignRole(user.getId(), role.getId());
-            }
-        } catch (SQLException e) {
-            throw new DaoException("Failed to assign role", e);
+            return roleDao.getUserRoles(user.getId());
+        } catch (DaoException e) {
+            return new ArrayList<>();
         }
     }
 
     @Override
-    public void assignDefaultRoles(Long userId) throws DaoException {
+    @Transactional
+    public void assignRoles(UserDto user, List<RoleDto> roles) throws ServiceException {
+        try {
+            for (RoleDto role : roles) {
+                roleDao.assignRole(user.getId(), role.getId());
+            }
+        } catch (SQLException | DaoException e) {
+            throw new ServiceException("Failed to assign role", e);
+        }
+    }
+
+    @Override
+    public void assignDefaultRoles(Long userId) throws ServiceException {
         try {
             roleDao.assignDefaultRoles(userId);
-        } catch (SQLException e) {
-            throw new DaoException("Failed to assign default roles", e);
+        } catch (SQLException | DaoException e) {
+            throw new ServiceException("Failed to assign default roles", e);
         }
     }
 
@@ -98,17 +106,43 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public boolean updateAssignUserRole(Long userId, String roleName) {
         try {
-            Optional<RoleDto> roleDto = roleDao.findAll().stream()
+            Optional<RoleDto> roleDto = roleDao.findAll()
+                    .stream()
                     .filter(role -> role.getRoleName().equalsIgnoreCase(roleName))
                     .findFirst();
             if (roleDto.isPresent()) {
                 roleDao.deleteAssignRoles(userId);
                 roleDao.assignRole(roleDto.get().getId(), userId);
+                if (roleName.trim().equalsIgnoreCase(ApplicationConstant.MANUFACTURER_ROLE)) {
+                    saveManufacture(userId);
+                }
                 return true;
             }
             return false;
         } catch (DaoException | SQLException e) {
             log.error("Failed to update assign role " + roleName, e);
+            return false;
+        }
+    }
+
+    private boolean saveManufacture(Long userId) {
+        try {
+            UserDto userDto = userDao.getById(userId);
+            if (userDto != null) {
+                Optional<ManufactureDto> manufactureOptional = manufactureDao.getByUserId(userDto.getId());
+                if (!manufactureOptional.isPresent()) {
+                    ManufactureDto manufactureDto = ManufactureDto.builder()
+                            .userId(userId)
+                            .name(userDto.getName())
+                            .build();
+                    manufactureDao.save(manufactureDto);
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } catch (DaoException e) {
+            log.error("Failed to save manufacture");
             return false;
         }
     }
